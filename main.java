9f46b4bcd8151201e8c1257e60290b47c72584ad
mariosmagid <mariosmagid@gmail.com>
2025-03-01 20:22:29 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,40 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+class main{
+    public static void main(String[] args) {
+       Scanner scanner  = new Scanner(System.in);
+       LocalTime alarm = null;
+       while(alarm == null)
+       {
+       try{
+       System.out.print("Enter The alarm Time (HH:mm:ss): ");
+       String inputTime = scanner.nextLine();
+       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+       alarm = LocalTime.parse(inputTime,formatter);
+       }
+       catch(DateTimeParseException e)
+       {
+            System.out.println("Invalid format use (HH:mm:ss)");
+       }
+       }
+       String filePath = "my own personal jesus.wav";
+       AlarmClock alarmClock = new AlarmClock(alarm,filePath,scanner);
+       Thread thread = new Thread(alarmClock);
+       thread.start();
+
+
+
+
+
+
+      
+      
+    }
+            
+                
+       
+          
+}
